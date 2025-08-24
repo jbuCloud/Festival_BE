7ae492cacde3ada407f681775d0dc7eff594de9d
박재중 <jjp910123@gmail.com>
2025-08-25 01:54:35 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/jbucloud/festival/FestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbucloud.festival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
